89880b328f72fc712d07a075ca7f01222eedb4ad
xiaobai721 <754112387@qq.com>
2017-06-26 10:05:26 +0800

    修复打包导致的conf路径问题

--- parseConfig.py
+++ parseConfig.py
@@ -1,10 +1,21 @@
 #-*- coding: utf-8 -*-
 import configparser
 import os
-
+from module_mylog import gLogger
 
 def getConfig(section, key):
     config = configparser.ConfigParser()
-    path = os.path.split(os.path.realpath(__file__))[0] + '/wind.conf'
+    # path = os.path.split(os.path.realpath(__file__))[0] + '/wind.conf'
+    path = resource_path("wind.conf")
+    gLogger.info(path)
     config.read(path)
     return config.get(section, key)
+
+def resource_path(relative):
+    return os.path.join(
+        os.environ.get(
+            "_MEIPASS2",
+            os.path.abspath(".")
+        ),
+        relative
+    )
